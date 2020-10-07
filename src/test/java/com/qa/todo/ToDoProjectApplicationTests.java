19468925d6558b2ef28cf66b86956cb7e194d40d
Simon Powell <spowell@qa.com>
2020-10-07 15:11:21 +0100

    Added src/test/java files

--- src/test/java/com/qa/todo/ToDoProjectApplicationTests.java
+++ src/test/java/com/qa/todo/ToDoProjectApplicationTests.java
@@ -1,13 +1,14 @@
-package com.qa.demo;
+package com.qa.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ToDoProjectApplicationTests {
-
+class ToDoProjectApplicationTests 
+{
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		
 	}
-
 }
