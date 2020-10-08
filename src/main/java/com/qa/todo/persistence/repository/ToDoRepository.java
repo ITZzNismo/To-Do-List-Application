8fdc350dce372bf16e24cf3ceddf975425631376
Simon Powell <spowell@qa.com>
2020-10-08 15:18:25 +0100

    Updated ToDo exception, domain, Repository, Controller, Service

--- src/main/java/com/qa/todo/persistence/repository/ToDoRepository.java
+++ src/main/java/com/qa/todo/persistence/repository/ToDoRepository.java
@@ -1,5 +1,12 @@
 package com.qa.todo.persistence.repository;
 
-public class ToDoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.todo.persistence.domain.ToDo;
+
+@Repository
+public interface ToDoRepository extends JpaRepository<ToDo, Long>
+{
 
 }
